f35cedf1da760a77ef972addcef8d3b77ee2026e
Bevzyk Soi <bevzyk.soi@gmail.com>
2021-12-06 19:31:11 +0200

    Sort added for serch results

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,8 @@
 'use strict'
 
-import searchResult from "../templates/search-result-page.hbs"
+import searchResult from "../templates/search-result-page.hbs";
+
+let results;
 
 const params = new URLSearchParams(window.location.search);
 let title = params.get('query');
@@ -12,7 +14,8 @@ const refs = {
     searchSection: document.querySelector(".search-res-section"),
     requestTitle: document.querySelector(".request"),
     pageChanger: document.querySelector(".page-list"),
-    pageChangerEl: document.querySelector(".page-list").getElementsByTagName("li"), 
+    pageChangerEl: document.querySelector(".page-list").getElementsByTagName("li"),
+    sortCont: document.querySelector("#sort")
 }
 
 setTimeout(() => {
@@ -34,7 +37,8 @@ fetch(`${MOVIE_API}search/movie?api_key=${API_KEY}&language=en-US&query=${title}
     .then(res => res.json())
     .then(result => {
         refs.requestTitle.textContent = title;
-        result.results.forEach(film => {
+        results = result.results;
+        results.forEach(film => {
             if (!film.poster_path) {
                 film.poster_path = "https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg";
                 film.posterClass = "noposter";
@@ -49,12 +53,11 @@ fetch(`${MOVIE_API}search/movie?api_key=${API_KEY}&language=en-US&query=${title}
                 film.release_year = new Date(film.release_date).getFullYear();
             }
         });
-        const markup = searchResult(result.results);
+        const markup = searchResult(results);
         refs.searchSection.innerHTML = markup;
 
         if(result.total_pages < 8) {
             pagesLimit = result.total_pages;
-            console.log(pagesLimit);
         }
 
         for(let i = 1; i <= pagesLimit; i++) {
@@ -63,5 +66,51 @@ fetch(`${MOVIE_API}search/movie?api_key=${API_KEY}&language=en-US&query=${title}
         }
     })
     .catch(error => console.error(error));
+  
 
-    
+refs.sortCont.addEventListener("change", event => {
+    const filmList = event.target.parentNode.parentNode.nextElementSibling;
+    filmList.innerHTML = '';
+    switch (event.target.value) {
+      case "popularity.desc":
+        results = results.sort((a, b) => {
+          return b.popularity - a.popularity;
+        });
+        break;
+
+      case "popularity.asc":
+        results = results.sort((a, b) => {
+          return a.popularity - b.popularity;
+        });
+        break;
+
+      case "original_title.desc":
+        results = results.sort((a, b) => {
+          return -a.title.localeCompare(b.title);
+        });
+        break;
+
+      case 'original_title.asc':
+        results = results.sort((a, b) => {
+          return a.title.localeCompare(b.title);
+        });
+        break;
+
+      case 'vote_average.desc':
+        results = results.sort((a, b) => {
+          return b.vote_average - a.vote_average;
+        });
+        break;
+
+      case 'vote_average.asc':
+        results = results.sort((a, b) => {
+          return a.vote_average - b.vote_average;
+        });
+        break;
+
+      default:
+        break;
+    }
+    const markup = searchResult(results);
+    filmList.insertAdjacentHTML("beforeend", markup);
+  });
