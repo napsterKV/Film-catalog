cf2f37aeca96ca0e2c0a416fde4e8636b35463e4
Bevzyk Soi <bevzyk.soi@gmail.com>
2021-12-08 19:13:31 +0200

    Fixed border for cast list

--- src/js/toolbar.js
+++ src/js/toolbar.js
@@ -1,4 +1,6 @@
-const { stubFalse } = require("lodash");
+const {
+  stubFalse
+} = require("lodash");
 
 const Theme = {
   LIGHT: 'light-theme',
@@ -8,17 +10,17 @@ const Theme = {
 const defaultTheme = Theme.LIGHT;
 
 function getTheme() {
-    let theme = localStorage.getItem("theme");
-    if (!theme) {
-        theme = defaultTheme;
-        setTheme(theme);
-    }
+  let theme = localStorage.getItem("theme");
+  if (!theme) {
+    theme = defaultTheme;
+    setTheme(theme);
+  }
 
-    return theme;
+  return theme;
 }
 
 function setTheme(theme) {
-    localStorage.setItem("theme", theme);
+  localStorage.setItem("theme", theme);
 }
 
 const checkbox = document.querySelector("#theme-switch-toggle");
@@ -27,84 +29,86 @@ const interfaceObject = Array.from(document.querySelectorAll(".interface-object"
 
 let currentTheme = getTheme();
 if (currentTheme === Theme.DARK) {
-    checkbox.setAttribute("checked", true);
-    interfaceObject.forEach(obj => obj.className = "interface-obj-dark")
-    setTimeout(() => {
-        const filmCardTitle = document.querySelectorAll(".film-card-title");
-        const searchSectionCard = document.querySelectorAll(".search-res-section .desc");
-        const castList = document.querySelectorAll(".cast-list");
-        const pageList = document.querySelectorAll(".page-list-el");
-        filmCardTitle.forEach(title => {
-            title.style.color = 'white';
-        })
-        searchSectionCard.forEach(card => {
-            card.style.borderColor = "white";
-        });
-        pageList.forEach(item => {
-            if(item.classList.contains('active')) {
-                item.children[0].style.color = "white";
-            }else{
-                item.style.borderColor = "white";
-                item.children[0].style.color = "white";
-            }
-            
-        });
-        castList.forEach(cast => {
-            cast.style.borderColor = "white";
-        });
-    }, 600)
-}else{
-    interfaceObject.forEach(obj => obj.className = "interface-obj-light")
+  checkbox.setAttribute("checked", true);
+  interfaceObject.forEach(obj => obj.className = "interface-obj-dark")
+  setTimeout(() => {
+    const filmCardTitle = document.querySelectorAll(".film-card-title");
+    const searchSectionCard = document.querySelectorAll(".search-res-section .desc");
+    const castList = document.querySelectorAll(".cast-list");
+    const pageList = document.querySelectorAll(".page-list-el");
+    filmCardTitle.forEach(title => {
+      title.style.color = 'white';
+    })
+    searchSectionCard.forEach(card => {
+      card.style.borderColor = "white";
+    });
+    pageList.forEach(item => {
+      if (item.classList.contains('active')) {
+        item.children[0].style.color = "white";
+      } else {
+        item.style.borderColor = "white";
+        item.children[0].style.color = "white";
+      }
+
+    });
+    castList.forEach(cast => {
+      cast.style.borderColor = "white";
+    });
+  }, 600)
+} else {
+  interfaceObject.forEach(obj => obj.className = "interface-obj-light")
 }
 
 body.classList.add(currentTheme);
 
 checkbox.addEventListener("change", event => {
-    body.classList.remove(currentTheme);
-    currentTheme = currentTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
-    body.classList.add(currentTheme);
-    setTheme(currentTheme);
-    interfaceObject.forEach(obj => {
-        obj.className = "";
-        if(currentTheme === Theme.DARK){
-            obj.className = "interface-obj-dark"
-        }else{
-            obj.className = "interface-obj-light"
-        }
-    });
-    const filmCardTitle = document.querySelectorAll(".film-card-title");
-    const searchSectionCard = document.querySelectorAll(".search-res-section .desc");
-    const pageList = document.querySelectorAll(".page-list-el");
-    filmCardTitle.forEach(title => {
-        if(currentTheme === Theme.DARK) {
-            title.style.color = 'white';
-        }else{
-            title.style.color = 'black';
-        }
-    })
-    searchSectionCard.forEach(card => {
-        if(currentTheme === Theme.DARK) {
-            card.style.borderColor = 'white';
-        }else{
-            card.style.borderColor = 'black';
-        }
-    });
-    pageList.forEach(item => {
-            if(currentTheme === Theme.DARK && !item.classList.contains('active')) {
-                item.style.borderColor = 'white';
-                item.children[0].style.color = "white";
-    
-            }else if(currentTheme === Theme.DARK && item.classList.contains('active')) {
-                item.children[0].style.color = "white";
+  body.classList.remove(currentTheme);
+  currentTheme = currentTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+  body.classList.add(currentTheme);
+  setTheme(currentTheme);
+  interfaceObject.forEach(obj => {
+    obj.className = "";
+    if (currentTheme === Theme.DARK) {
+      obj.className = "interface-obj-dark"
+    } else {
+      obj.className = "interface-obj-light"
+    }
+  });
+  const filmCardTitle = document.querySelectorAll(".film-card-title");
+  const searchSectionCard = document.querySelectorAll(".search-res-section .desc");
+  const pageList = document.querySelectorAll(".page-list-el");
+  const castList = document.querySelectorAll(".cast-list");
+  filmCardTitle.forEach(title => {
+    if (currentTheme === Theme.DARK) {
+      title.style.color = 'white';
+    } else {
+      title.style.color = 'black';
+    }
+  })
+  searchSectionCard.forEach(card => {
+    if (currentTheme === Theme.DARK) {
+      card.style.borderColor = 'white';
+    } else {
+      card.style.borderColor = 'black';
+    }
+  });
+  castList.forEach(cast => {
+    cast.style.borderColor = "white";
+  });
+  pageList.forEach(item => {
+    if (currentTheme === Theme.DARK && !item.classList.contains('active')) {
+      item.style.borderColor = 'white';
+      item.children[0].style.color = "white";
 
-            }else if(currentTheme === Theme.LIGHT && !item.classList.contains('active')){
-                item.style.borderColor = 'black';
-                item.children[0].style.color = "black";
-            }else if (currentTheme === Theme.LIGHT && item.classList.contains('active')) {
-                item.children[0].style.color = "black";
-            }
-            
-    });
+    } else if (currentTheme === Theme.DARK && item.classList.contains('active')) {
+      item.children[0].style.color = "white";
+
+    } else if (currentTheme === Theme.LIGHT && !item.classList.contains('active')) {
+      item.style.borderColor = 'black';
+      item.children[0].style.color = "black";
+    } else if (currentTheme === Theme.LIGHT && item.classList.contains('active')) {
+      item.children[0].style.color = "black";
+    }
+
+  });
 })
-
-
